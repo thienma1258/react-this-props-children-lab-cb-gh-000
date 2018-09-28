e3601b1785e2aa9ae1146caa508942e2e500f20e
Ngoc Dong Pham <cpud1258@gmail.com>
2018-09-28 03:55:51 +0000

    Done.

--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,23 @@
-// Code ThemedDecoration Component Here
+// Code Invitation Component Here
+import React from 'react';
+
+class ThemedDecorations extends React.Component
+{
+render(){
+  return(
+<div>
+{this.props.children.map(item=>{
+  const props = { ...item.props, className: this.props.theme };
+return React.cloneElement(item, props, "TEST");
+
+
+})}
+</div>
+
+
+
+  )
+}
+
+}
+export default ThemedDecorations;
