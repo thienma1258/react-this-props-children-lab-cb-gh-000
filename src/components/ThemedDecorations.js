c4dfcdb0a04961505efc6ebef407528892821256
Ngoc Dong Pham <cpud1258@gmail.com>
2018-09-28 03:46:07 +0000

    automatically backed up by learn

--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,18 @@
-// Code ThemedDecoration Component Here
+// Code Invitation Component Here
+import React from 'react';
+
+class ThemedDecorations extends React.Component
+{
+render(){
+  return(
+<div>
+{this.props.children}
+</div>
+
+
+    
+  )
+}
+  
+}
+export default ThemedDecorations;
